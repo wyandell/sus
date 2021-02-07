efc5fc82e21793f84012bf0ac52418ef47a60ac2
buizel <27046219+Pokemonjpups@users.noreply.github.com>
2021-02-07 14:03:05 -0500

    [remove] unused go server

--- avatar-render-go/controller/health-check.go
+++
@@ -1,7 +0,0 @@
-package controller
-
-import "github.com/gofiber/fiber"
-
-func HealthCheck(c *fiber.Ctx) {
-	c.JSON(fiber.Map{"success": true})
-}
--- avatar-render-go/go.mod
+++
@@ -1,8 +0,0 @@
-module avatar-render
-
-go 1.14
-
-require (
-	github.com/gofiber/fiber v1.13.3
-	github.com/gofiber/logger v0.2.4
-)
